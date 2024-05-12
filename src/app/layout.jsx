55eedfff27909a29504ea223258fd761233a3196
Akshay Kakatkar <akshayakakatkaraa@gmail.com>
2024-05-12 11:51:08 +0530

    Setup NextUI
    * Install nextui and framer motion
    * Setup a providers folder for wrapping all client providers
    * Setup nextui plugin for tailwind

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,6 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Nabvar from "@/components/Nabvar";
+import Providers from "@/providers";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -13,8 +14,10 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Nabvar />
-        {children}
+        <Providers>
+          <Nabvar />
+          {children}
+        </Providers>
       </body>
     </html>
   );
