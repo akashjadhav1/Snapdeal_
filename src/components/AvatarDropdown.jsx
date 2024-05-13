f7bf376ecc55f4cbed2304b9ac6825756489df4c
Akshay Kakatkar <akshayakakatkaraa@gmail.com>
2024-05-13 12:54:16 +0530

    Only load dropdown menu once details are fetched

--- src/components/AvatarDropdown.jsx
+++ src/components/AvatarDropdown.jsx
@@ -37,24 +37,26 @@ export default function AvatarDropdown() {
           src={user.data?.photoURL || <Skeleton />}
         />
       </DropdownTrigger>
-      <DropdownMenu
-        onMouseLeave={() => {
-          setIsOpen(false);
-        }}
-        aria-label="Profile Actions"
-        variant="flat"
-      >
-        <DropdownItem key="profile" className="h-14 gap-2">
-          <p className="font-semibold">Signed in as</p>
-          <p className="font-semibold">{user.data?.email}</p>
-        </DropdownItem>
-        <DropdownItem key="account">Your Account</DropdownItem>
-        <DropdownItem key="orders">Your Orders</DropdownItem>
-        <DropdownItem key="shortlist">Your Shortlist</DropdownItem>
-        <DropdownItem key="logout" color="danger" onClick={handleLogout}>
-          Log Out
-        </DropdownItem>
-      </DropdownMenu>
+      {user.isAuthenticated && (
+        <DropdownMenu
+          onMouseLeave={() => {
+            setIsOpen(false);
+          }}
+          aria-label="Profile Actions"
+          variant="flat"
+        >
+          <DropdownItem key="profile" className="h-14 gap-2">
+            <p className="font-semibold">Signed in as</p>
+            <p className="font-semibold">{user.data?.email}</p>
+          </DropdownItem>
+          <DropdownItem key="account">Your Account</DropdownItem>
+          <DropdownItem key="orders">Your Orders</DropdownItem>
+          <DropdownItem key="shortlist">Your Shortlist</DropdownItem>
+          <DropdownItem key="logout" color="danger" onClick={handleLogout}>
+            Log Out
+          </DropdownItem>
+        </DropdownMenu>
+      )}
     </Dropdown>
   );
 }
