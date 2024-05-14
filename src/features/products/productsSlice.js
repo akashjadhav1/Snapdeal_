ad63a9c9a8145bd8884197a0dbd786e24910b740
Akshay Kakatkar <akshayakakatkaraa@gmail.com>
2024-05-14 22:30:57 +0530

    Handle product fetching logic in redux slice

--- src/features/products/productsSlice.js
+++ src/features/products/productsSlice.js
@@ -1,30 +1,84 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { collection, getDocs, query, where } from "firebase/firestore";
+import { db } from "@/config/firebase";
+
+export const fetchData = createAsyncThunk(
+  "products/fetchData",
+  async (_, { getState }) => {
+    try {
+      const state = getState();
+      const { filters } = state.products;
+
+      const productsCollection = collection(db, "products");
+
+      const filtersArray = [];
+      if (filters.category)
+        filtersArray.push(where("category", "==", filters.category));
+      if (filters.subcategory)
+        filtersArray.push(where("subcategory", "==", filters.subcategory));
+      if (filters.minPrice)
+        filtersArray.push(where("price", ">=", filters.minPrice));
+      if (filters.maxPrice)
+        filtersArray.push(where("price", "<=", filters.maxPrice));
+
+      const q = query(productsCollection, ...filtersArray);
+      const snapshot = await getDocs(q);
+
+      const data = snapshot.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+      }));
+
+      return data;
+    } catch (error) {
+      throw new Error(error.message);
+    }
+  }
+);
 
 const productsSlice = createSlice({
   name: "products",
   initialState: {
-    data: null,
-    filters: [],
+    list: [],
+    filters: {
+      category: null,
+      subcategory: null,
+      minPrice: null,
+      maxPrice: null,
+    },
     loading: false,
     error: null,
-    status: "idle",
   },
   reducers: {
-    fetchDataStart(state) {
-      if (state.status === "idle") state.loading = true;
-      state.error = null;
+    setCategory(state, action) {
+      state.filters.category = action.payload;
     },
-    fetchDataSuccess(state, action) {
-      state.loading = false;
-      state.data = action.payload;
+    setSubcategory(state, action) {
+      state.filters.subcategory = action.payload;
     },
-    fetchDataFailure(state, action) {
-      state.loading = false;
-      state.error = action.payload;
+    setMinPrice(state, action) {
+      state.filters.minPrice = action.payload;
     },
+    setMaxPrice(state, action) {
+      state.filters.maxPrice = action.payload;
+    },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchData.pending, (state) => {
+        state.loading = true;
+      })
+      .addCase(fetchData.fulfilled, (state, action) => {
+        state.loading = false;
+        state.list = action.payload;
+      })
+      .addCase(fetchData.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.error.message;
+      });
   },
 });
 
-export const { fetchDataStart, fetchDataSuccess, fetchDataFailure } =
+export const { setCategory, setSubcategory, setMinPrice, setMaxPrice } =
   productsSlice.actions;
 export default productsSlice.reducer;
--- src/providers/redux/ReduxLoader.jsx
+++ src/providers/redux/ReduxLoader.jsx
@@ -1,12 +1,6 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { collection, getDocs, query, where } from "firebase/firestore";
-import { db } from "@/config/firebase";
-import {
-  fetchDataFailure,
-  fetchDataStart,
-  fetchDataSuccess,
-} from "@/features/products/productsSlice";
+import { fetchData, setMinPrice } from "@/features/products/productsSlice";
 import { fetchUser } from "@/features/user/userSlice";
 
 export default function ReduxLoader() {
@@ -14,39 +8,13 @@ export default function ReduxLoader() {
   const filters = useSelector((state) => state.products.filters);
 
   useEffect(() => {
-    dispatch(fetchUser());
+    const unsubscribe = () => dispatch(fetchUser());
+
+    return () => unsubscribe();
   }, [dispatch]);
 
   useEffect(() => {
-    const unsubscribe = async () => {
-      dispatch(fetchDataStart());
-      try {
-        const productsCollection = collection(db, "products");
-
-        const filtersArray = [];
-        if (filters.category)
-          filtersArray.push(where("category", "==", filters.category));
-        if (filters.subcategory)
-          filtersArray.push(where("subcategory", "==", filters.subcategory));
-        if (filters.minPrice)
-          filtersArray.push(where("price", ">=", filters.minPrice));
-        if (filters.maxPrice)
-          filtersArray.push(where("price", "<=", filters.maxPrice));
-
-        const q = query(productsCollection, ...filtersArray);
-        const snapshot = await getDocs(q);
-
-        const data = snapshot.docs.map((doc) => ({
-          id: doc.id,
-          ...doc.data(),
-        }));
-        dispatch(fetchDataSuccess(data));
-      } catch (error) {
-        dispatch(fetchDataFailure(error.message));
-      }
-    };
-
-    unsubscribe();
+    const unsubscribe = () => dispatch(fetchData());
 
     return () => unsubscribe();
   }, [filters, dispatch]);
