ae5981e26edf293289bb8a6c2911f3790ef6a13c
Akash Jadhav <akashjadhav5975@gmail.com>
2024-05-19 11:50:38 +0530

    add mvp

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -54,6 +54,44 @@ export default function Products({ filters = {} }) {
               ></Button>
             </CardFooter>
           </Card>
+<<<<<<< HEAD
+=======
+          //   <Card
+          //   className="max-w-[400px] cursor-pointer hover:shadow-xl transition-shadow duration-300"
+          //   onClick={handleCardClick}
+          // >
+          //   <CardHeader className="relative flex items-center justify-center h-64">
+          //     <Image
+          //       alt={product.title}
+          //       src={product.image}
+          //       classNames={{
+          //         wrapper: "object-contain h-full bg-white",
+          //         img: "object-contain w-full h-full",
+          //       }}
+          //     />
+          //     <ShortListHeart productId={product.id} />
+          //   </CardHeader>
+          //   <CardBody className="flex flex-col space-y-2">
+          //     <div className="flex justify-between">
+          //       <p className="text-md text-black w-1/2 truncate">{product.title}</p>
+          //       <p className="text-md text-gray-700">Rs. {product.price}</p>
+          //     </div>
+          //     <div className="flex text-[gold] text-xs">
+          //       {renderStars(Math.round(product.rating.rate))}
+          //     </div>
+          //   </CardBody>
+          // <CardFooter>
+          //   <Button
+          //     size="small"
+          //     className="bg-blue-700 text-white hover:bg-blue-800 w-full"
+          //     disabled={isAddedToCart}
+          //     onClick={handleAddToCart}
+          //   >
+          //     Add to Cart
+          //   </Button>
+          // </CardFooter>
+          // </Card>
+>>>>>>> ee8f80a (Show skeleton while products are loading)
         ))}
       </div>
     );
